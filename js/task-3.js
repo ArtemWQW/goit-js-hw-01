0cfeaba9d850aff8a9f0eac5a3ef7aeaf6e2c258
ArtemWQW <149912826+ArtemWQW@users.noreply.github.com>
2024-03-13 15:23:38 +0200

    task-3

--- js/task-3.js
+++ js/task-3.js
@@ -1,5 +1,11 @@
 function getElementWidth (content, padding, border) {
 
+    console.log(Number.parseFloat(content));
+    console.log(Number.parseFloat(padding));
+    console.log(Number.parseFloat(border));
+
+    return `${content} + ${padding * 2} + ${border * 2}`
+
 };
 
 console.log(getElementWidth("50px", "8px", "4px"));
